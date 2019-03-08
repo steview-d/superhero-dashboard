e303adfce06dd75083ac676c4fc2cf9e74900843
Dave Laffan <dave.laffan@outlook.com>
2019-03-08 16:21:19 +0000

    Add 2 charts for testing purposes
    Upload test data and create 1 pie and 1 row chart to test with

--- static/js/data-vis.js
+++ static/js/data-vis.js
@@ -5,27 +5,61 @@ queue()
 function createDataVis(error, superheroData) {
     var ndx = crossfilter(superheroData);
 
+    //Tidy 
     superheroData.forEach(function(d) {
         d.skin_color = d["Skin color"];
     });
 
+    //superheroData.Alignment.filter(clean_alignment_data)
+
     gender_selector(ndx);
     test_graph_one(ndx);
     test_graph_two(ndx);
-
     dc.renderAll();
+}
 
+function remove_blanks(group, string_to_remove = "") {
+    // From within passed group, filter out all blank entries
+    return {
+        all: function() {
+            return group.all().filter(function(d) {
+                return d.key !== string_to_remove;
+            });
+        }
+    };
 }
 
 function gender_selector(ndx) {
+    dim = ndx.dimension(dc.pluck('Gender'));
+    group = dim.group()
 
+    dc.selectMenu('#gender-selector')
+        .dimension(dim)
+        .group(group);
 }
-
 
 function test_graph_one(ndx) {
 
+    var alignmentDim = ndx.dimension(function(d) { return d.Alignment; });
+    var alignmentGroup = remove_blanks(alignmentDim.group());
+    //var alignment_group = remove_blanks(alignment_group);
+
+    dc.pieChart('#graph-one')
+        .height(350)
+        .radius(130)
+        .transitionDuration(500)
+        .dimension(alignmentDim)
+        .group(alignmentGroup);
 }
 
 function test_graph_two(ndx) {
 
+    var skinColorDim = ndx.dimension(function(d) { return d.skin_color; });
+    var skinColorGroup = remove_blanks(skinColorDim.group());
+
+    dc.rowChart('#graph-two')
+        .width(800)
+        .height(800)
+        .dimension(skinColorDim)
+        .group(skinColorGroup);
 }
