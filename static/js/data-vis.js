510db7b88f237e8b9e7aa7b961ca68486658c366
Dave Laffan <dave.laffan@outlook.com>
2019-03-13 12:46:01 +0000

    Add all data-vis elements
    Add 2no Bar Charts (height & weight)
    Add 1no Row Chart (creator)
    Add 1no Line Chart (attributes)

--- static/js/data-vis.js
+++ static/js/data-vis.js
@@ -1,3 +1,5 @@
+/* --------------------------------------------------------- Main -*/
+
 queue()
     .defer(d3.csv, 'static/data/superheroData.csv')
     .await(createDataVis);
@@ -5,36 +7,86 @@ queue()
 function createDataVis(error, superheroData) {
     var ndx = crossfilter(superheroData);
 
-    // MIGHT NOT BE REQUIRED - TBC
-    // Allow script to read data with blanks in headers
-    // superheroData.forEach(function(d) {
-    //     d.skin_color = d["Skin color"];
-    //     d.hair_color = d["Hair color"];
-    //     d.alter_ego = d["Alter Egos"];
-    // });
+    /* ---------- Data Preparation -------*/
 
-    // Graphs & Charts & Pies, etc
+    var heroAttributes = [
+        "Intelligence",
+        "Strength",
+        "Speed",
+        "Durability",
+        "Power",
+        "Combat"
+    ];
+    
+    function hero_attr_to_integer(hero_attr) {
+        // Convert all hero attributes to integers. Store as a
+        // lowercased variable of the same name.
+        hero_attr.forEach(function(ha) {
+            superheroData.forEach(function(d) {
+                var ha_lower = ha.toLowerCase();
+                d[ha_lower] = parseInt(d[ha], 10);
+            });
+        });
+    }
+    hero_attr_to_integer(heroAttributes);
+
+    function height_weight_conversion(key, firstIndex, lastIndex) {
+        // Iterate through the given key to remove unwanted chars from the
+        // values and store an integer as a lowercased variable of same name.
+        superheroData.forEach(function(d) {
+            var keyLower = key.toLowerCase();
+            d[keyLower] = parseInt(
+                d[key].substring(
+                    d[key].lastIndexOf(firstIndex) + firstIndex.length,
+                    d[key].lastIndexOf(lastIndex)), 10);
+        });
+    }
+    height_weight_conversion("Height", "// ", " c");
+    height_weight_conversion("Weight", "// ", " k");
+
+
+    /* ---------- Gender -----------------*/
     gender_selector(ndx);
-    alignment(ndx);
-    alter_ego(ndx);
-    skin_color(ndx);
-    hair_color(ndx);
-    eye_color(ndx);
+    display_gender_percent(ndx, 'Male', '#percent-male');
+    display_gender_percent(ndx, 'Female', '#percent-female');
+    display_gender_percent(ndx, 'Other', '#percent-other');
 
+    /* ---------- Pie Charts -------------*/
+    // alignment(ndx);
+    // alter_ego(ndx);
+    // skin_color(ndx);
+    // hair_color(ndx);
+    // eye_color(ndx);
+
+    /* ---------- Bar Charts -------------*/
+    // height(ndx);
+    // weight(ndx);
+
+    /* ---------- Row Chart --------------*/
+    // creator(ndx);
+
+    /* ---------- Line Chart -------------*/
+    stats(ndx);
 
     dc.renderAll();
 }
 
-function remove_blanks(group, string_to_remove = "") {
-    // From within passed group, filter out all blank entries
+
+/* --------------------------------------------- Helper Functions -*/
+
+function remove_blanks(group, value_to_remove = "") {
+    // Filter out specified values from passed group
     return {
         all: function() {
             return group.all().filter(function(d) {
-                return d.key !== string_to_remove;
+                return d.key !== value_to_remove;
             });
         }
     };
 }
+
+
+/* --------------------------------------------- Gender Functions -*/
 
 function gender_selector(ndx) {
     dim = ndx.dimension(dc.pluck('Gender'));
@@ -45,11 +97,47 @@ function gender_selector(ndx) {
         .group(group);
 }
 
+function display_gender_percent(ndx, gender, element) {
+    var genderPercent = ndx.groupAll().reduce(
+        function(p, v) {
+            p.total++;
+            if (v.Gender === gender) {
+                p.gender_count++;
+            }
+            return p;
+        },
+        function(p, v) {
+            p.total--;
+            if (v.Gender === gender) {
+                p.gender_count--;
+            }
+            return p;
+        },
+        function() {
+            return { total: 0, gender_count: 0 };
+        }
+    );
+
+    dc.numberDisplay(element)
+        .formatNumber(d3.format('.2%'))
+        .valueAccessor(function(d) {
+            if (d.gender_count == 0) {
+                return 0;
+            }
+            else {
+                return (d.gender_count / d.total);
+            }
+        })
+        .group(genderPercent)
+}
+
+
+/* --------------------------------------------------- Pie Charts -*/
+
 function alignment(ndx) {
 
     var alignmentDim = ndx.dimension(dc.pluck('Alignment'));
     var alignmentGroup = remove_blanks(alignmentDim.group());
-    //var alignment_group = remove_blanks(alignment_group);
 
     dc.pieChart('#alignment')
         .height(350)
@@ -106,4 +194,138 @@ function eye_color(ndx) {
         .radius(130)
         .dimension(eyeColorDim)
         .group(eyeColorGroup);
+}
+
+
+/* --------------------------------------------------- Bar Charts -*/
+
+function height(ndx) {
+    var heightDim = ndx.dimension(function(d) {
+        switch (true) {
+            case (d.height == 0):
+                return "0m";
+            case (d.height < 100):
+                return "0m to 1m";
+            case (d.height < 200):
+                return "1m to 2m";
+            case (d.height < 300):
+                return "2m to 3m";
+            case (d.height >= 300):
+                return "Over 3m";
+        }
+    });
+
+    var heightGroup = remove_blanks(heightDim.group(), "0m");
+
+    dc.barChart('#height')
+        .width(600)
+        .height(400)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(heightDim)
+        .group(heightGroup)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel('Height')
+        .yAxis().ticks(4);
+}
+
+function weight(ndx) {
+    var weightDim = ndx.dimension(function(d) {
+        switch (true) {
+            case (d.weight == 0):
+                return "0kg";
+            case (d.weight < 60):
+                return "0kg to 59kg";
+            case (d.weight < 90):
+                return "60kg to 89kg";
+            case (d.weight < 120):
+                return "90kg to 119kg";
+            case (d.weight >= 120):
+                return "Over 120kg";
+        }
+    });
+
+    var weightGroup = remove_blanks(weightDim.group(), "0kg");
+
+    dc.barChart('#weight')
+        .width(600)
+        .height(400)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(weightDim)
+        .group(weightGroup)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .xAxisLabel('Weight')
+        .yAxis().ticks(4);
+
+}
+
+
+/* --------------------------------------------------- Row Charts -*/
+
+function creator(ndx) {
+    var creatorDim = ndx.dimension(dc.pluck('Creator'));
+    var creatorGroup = remove_blanks(creatorDim.group());
+
+    dc.rowChart('#creator')
+        .width(900)
+        .height(400)
+        .dimension(creatorDim)
+        .group(creatorGroup)
+        .transitionDuration(500);
+}
+
+
+/* -------------------------------------------------- Line Charts -*/
+
+function stats(ndx) {
+
+    var intDim = ndx.dimension(dc.pluck('intelligence'));
+    var intGroup = remove_blanks(intDim.group(), 0);
+    var strDim = ndx.dimension(dc.pluck('strength'));
+    var strGroup = remove_blanks(strDim.group(), 0);
+    var speDim = ndx.dimension(dc.pluck('speed'));
+    var speGroup = remove_blanks(speDim.group(), 0);
+    var durDim = ndx.dimension(dc.pluck('durability'));
+    var durGroup = remove_blanks(durDim.group(), 0);
+    var powDim = ndx.dimension(dc.pluck('power'));
+    var powGroup = remove_blanks(powDim.group(), 0);
+    var comDim = ndx.dimension(dc.pluck('combat'));
+    var comGroup = remove_blanks(comDim.group(), 0);
+
+    var compositeChart = dc.compositeChart('#stats');
+
+    compositeChart
+        .width(1000)
+        .height(400)
+        .margins({ top: 10, right: 30, bottom: 30, left: 30 })
+        .x(d3.scale.linear().domain([0, 100]))
+        .xAxisLabel('Attribute Value')
+        .yAxisLabel('Frequency')
+        .elasticY(true)
+        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+        .compose([
+            dc.lineChart(compositeChart)
+            .colors('green')
+            .group(intGroup, 'Intelligence'),
+            dc.lineChart(compositeChart)
+            .colors('red')
+            .group(strGroup, 'Strength'),
+            dc.lineChart(compositeChart)
+            .colors('yellow')
+            .group(speGroup, 'Speed'),
+            dc.lineChart(compositeChart)
+            .colors('orange')
+            .group(durGroup, 'Durability'),
+            dc.lineChart(compositeChart)
+            .colors('blue')
+            .group(powGroup, 'Power'),
+            dc.lineChart(compositeChart)
+            .colors('purple')
+            .group(comGroup, 'Combat'),
+        ]);
 }
