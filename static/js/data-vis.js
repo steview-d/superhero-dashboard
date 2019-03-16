3138b8c8286647cadafd42a2860a1677d04e0634
Dave Laffan <dave.laffan@outlook.com>
2019-03-16 18:05:25 +0000

    Add initial html structure and make charts responsive

--- static/js/data-vis.js
+++ static/js/data-vis.js
@@ -46,15 +46,15 @@ function createDataVis(error, superheroData) {
 
 
     /* ---------- Gender -----------------*/
-    gender_selector(ndx);
-    display_gender_percent(ndx, 'Male', '#percent-male');
-    display_gender_percent(ndx, 'Female', '#percent-female');
-    display_gender_percent(ndx, 'Other', '#percent-other');
+    // gender_selector(ndx);
+    // display_gender_percent(ndx, 'Male', '#percent-male');
+    // display_gender_percent(ndx, 'Female', '#percent-female');
+    // display_gender_percent(ndx, 'Other', '#percent-other');
 
     /* ---------- Pie Charts -------------*/
     alignment(ndx);
     alter_ego(ndx);
-    skin_color(ndx);
+    // skin_color(ndx); // Too little data to be of much use
     hair_color(ndx);
     eye_color(ndx);
 
@@ -67,8 +67,9 @@ function createDataVis(error, superheroData) {
 
     /* ---------- Line Chart -------------*/
     stats(ndx);
-    
+
     dc.renderAll();
+
 }
 
 
@@ -137,38 +138,61 @@ function display_gender_percent(ndx, gender, element) {
 
 function alignment(ndx) {
 
+    var alignmentColors = d3.scale.ordinal()
+        .range(['#bbb', '#666', '#888']);
+
     var alignmentDim = ndx.dimension(dc.pluck('Alignment'));
     var alignmentGroup = remove_blanks(alignmentDim.group(), "");
 
     dc.pieChart('#alignment')
+        .width(500)
         .height(350)
-        .radius(130)
+        .radius(170)
         .transitionDuration(500)
+        .cx(210)
+        .legend(dc.legend().x(420).y(20).itemHeight(30).gap(8))
+        .colorAccessor(function(d) {
+            return d.key;
+        })
+        .colors(alignmentColors)
+        .useViewBoxResizing(true)
         .dimension(alignmentDim)
         .group(alignmentGroup);
 }
 
 function alter_ego(ndx) {
 
+    var alterEgoColors = d3.scale.ordinal()
+        .range(['indigo', 'deeppink']);
+
     var alterEgoDim = ndx.dimension(dc.pluck('Alter Egos'));
     var alterEgoGroup = remove_blanks(alterEgoDim.group(), "");
 
     dc.pieChart('#alter-ego')
+        .width(500)
         .height(350)
-        .radius(130)
+        .radius(170)
         .transitionDuration(500)
+        .cx(210)
+        .legend(dc.legend().x(420).y(20).itemHeight(30).gap(8))
+        .colorAccessor(function(d) {
+            return d.key;
+        })
+        .colors(alterEgoColors)
+        .useViewBoxResizing(true)
         .dimension(alterEgoDim)
         .group(alterEgoGroup);
 }
 
 function skin_color(ndx) {
-
+    // Most likely not using - too many blanks
     var skinColorDim = ndx.dimension(dc.pluck('Skin color'));
     var skinColorGroup = remove_blanks(skinColorDim.group(), "");
 
     dc.pieChart('#skin-color')
         .height(350)
         .radius(130)
+        .slicesCap(7)
         .dimension(skinColorDim)
         .group(skinColorGroup);
 }
@@ -179,10 +203,32 @@ function hair_color(ndx) {
     var hairColorGroup = remove_blanks(hairColorDim.group(), "");
 
     dc.pieChart('#hair-color')
+        .width(500)
         .height(350)
-        .radius(130)
+        .radius(170)
+        .transitionDuration(500)
+        .cx(210)
+        .slicesCap(6)
+        .legend(dc.legend().x(420).y(20).itemHeight(20).gap(5))
+        //.drawPaths(true)
+        // .externalRadiusPadding(30)
+        // .externalLabels(30)
+        // .minAngleForLabel(0)
+        .label(function(d) {
+            console.log(dc.utils.printSingleValue(d.endAngle, d.startAngle))
+            if (dc.utils.printSingleValue(d.endAngle - d.startAngle < 50000000000 * Math.PI / 180)) { return "sss" }
+            else {return "hjhjhj"}
+        })
+        // .on('pretransition', function(chart) {
+        //     chart.selectAll('text.pie-slice').text(function(d) {
+        //         // return dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100) + '%';
+        //         return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100) + '%';
+        //     })
+        // })
+        .useViewBoxResizing(true)
         .dimension(hairColorDim)
         .group(hairColorGroup);
+
 }
 
 function eye_color(ndx) {
@@ -191,8 +237,24 @@ function eye_color(ndx) {
     var eyeColorGroup = remove_blanks(eyeColorDim.group(), "");
 
     dc.pieChart('#eye-color')
+        .width(500)
         .height(350)
-        .radius(130)
+        .radius(170)
+        .transitionDuration(500)
+        .cx(210)
+        .slicesCap(7)
+        .legend(dc.legend().x(420).y(20).itemHeight(20).gap(5))
+        //.drawPaths(true)
+        // .externalRadiusPadding(30)
+        // .externalLabels(30)
+        // .minAngleForLabel(0)
+        // .on('pretransition', function(chart) {
+        //     chart.selectAll('text.pie-slice').text(function(d) {
+        //         // return dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100) + '%';
+        //         return d.data.key + ' ' + dc.utils.printSingleValue((d.endAngle - d.startAngle) / (2 * Math.PI) * 100) + '%';
+        //     })
+        // })
+        .useViewBoxResizing(true)
         .dimension(eyeColorDim)
         .group(eyeColorGroup);
 }
@@ -201,6 +263,10 @@ function eye_color(ndx) {
 /* --------------------------------------------------- Bar Charts -*/
 
 function height(ndx) {
+
+    var heightColors = d3.scale.ordinal()
+        .range(['pink', 'red']);
+
     var heightDim = ndx.dimension(function(d) {
         switch (true) {
             case (d.height == 0):
@@ -219,20 +285,29 @@ function height(ndx) {
     var heightGroup = remove_blanks(heightDim.group(), "0m");
 
     dc.barChart('#height')
-        .width(600)
-        .height(400)
+        .width(500)
+        .height(350)
+        .useViewBoxResizing(true)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(heightDim)
         .group(heightGroup)
-        .transitionDuration(500)
+        .colorAccessor(function(d) {
+            return d.key;
+        })
+        .colors(heightColors)
+        .elasticY(true)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
         .xAxisLabel('Height')
         .yAxis().ticks(4);
+
 }
 
 function weight(ndx) {
+
+    var weightColors = d3.scale.ordinal()
+        .range(['pink', 'red']);
+
     var weightDim = ndx.dimension(function(d) {
         switch (true) {
             case (d.weight == 0):
@@ -251,18 +326,21 @@ function weight(ndx) {
     var weightGroup = remove_blanks(weightDim.group(), "0kg");
 
     dc.barChart('#weight')
-        .width(600)
-        .height(400)
+        .width(500)
+        .height(350)
+        .useViewBoxResizing(true)
         .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(weightDim)
+        .colorAccessor(function(d) {
+            return d.key;
+        })
+        .colors(weightColors)
         .group(weightGroup)
-        .transitionDuration(500)
+        .elasticY(true)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .elasticY(true)
         .xAxisLabel('Weight')
         .yAxis().ticks(4);
-
 }
 
 
@@ -277,6 +355,8 @@ function creator(ndx) {
         .height(400)
         .dimension(creatorDim)
         .group(creatorGroup)
+        .cap(15)
+        .useViewBoxResizing(true)
         .transitionDuration(500);
 }
 
@@ -301,14 +381,15 @@ function stats(ndx) {
     var compositeChart = dc.compositeChart('#stats');
 
     compositeChart
-        .width(1000)
+        .width(900)
         .height(400)
         .margins({ top: 10, right: 30, bottom: 30, left: 30 })
         .x(d3.scale.linear().domain([0, 100]))
         .xAxisLabel('Attribute Value')
         .yAxisLabel('Frequency')
         .elasticY(true)
-        .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
+        .legend(dc.legend().x(80).y(20).itemHeight(18).gap(5).horizontal(true).autoItemWidth(true))
+        .useViewBoxResizing(true)
         .compose([
             dc.lineChart(compositeChart)
             .colors('green')
